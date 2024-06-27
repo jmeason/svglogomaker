f8821fae09605d608c4850438e7f4bbbbb7706b1
Julia Eason <jmeason00@gmail.com>
2024-06-26 22:48:19 -0400

    adding code to each file

---
+++ index.js
@@ -0,0 +1,63 @@
+const  inquirer= require('inquirer');
+const fs = require('fs');
+const circle= require ('./lib/circle');
+const square= require ('./lib/square');
+const triangle= require ('./lib/triangle');
+const Shapes=require ('.lib/shapes')
+
+
+async function promptUser() {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter text (up to 3 characters):',
+      validate: input => input.length <= 3 || 'Text must be 3 characters or less'
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (keyword or hexadecimal):'
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square']
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (keyword or hexadecimal):'
+    }
+  ]);
+
+  let shapeInstance;
+  switch (answers.shape) {
+    case 'circle':
+      shapeInstance = new Circle();
+      break;
+    case 'triangle':
+      shapeInstance = new Triangle();
+      break;
+    case 'square':
+      shapeInstance = new Square();
+      break;
+    default:
+      throw new Error('Invalid shape');
+  }
+
+  shapeInstance.setColor(answers.shapeColor);
+
+  const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shapeInstance.render()}
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+    </svg>
+  `;
+
+  fs.writeFileSync('logo.svg', svgContent.trim());
+  console.log('Generated logo.svg');
+}
+
+promptUser();
